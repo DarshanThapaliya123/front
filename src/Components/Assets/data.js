3edcbf2dc51f7a5146d890d23f24796d6b754aad
DarshanThapaliya123 <108362093+DarshanThapaliya123@users.noreply.github.com>
2025-02-08 01:23:08 +0545

    first commit

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,33 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product = [
+  {
+    id:1,
+    image:p1_img,
+    new_price:2500,
+    old_price:1850,
+  },
+  {
+    id:2,
+    image:p2_img,
+    new_price:995,
+    old_price:1250,
+  },
+  {
+    id:3,
+    image:p3_img,
+    new_price:60.00,
+    old_price:100.50,
+  },
+  {
+    id:4,
+    image:p4_img,
+    new_price:100.00,
+    old_price:150.00,
+  },
+];
+
+export default data_product;
