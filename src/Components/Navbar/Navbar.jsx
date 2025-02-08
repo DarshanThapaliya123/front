dafba4b09e7d2e3b45f1f34a8243b43754132362
DarshanThapaliya123 <108362093+DarshanThapaliya123@users.noreply.github.com>
2025-02-08 17:24:35 +0545

    second commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <li onClick={()=>{setMenu("kids")}}><Link to ='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
-       <Link TO='/login'><button>Login</button></Link> 
+       <Link to='/login'><button>Login</button></Link> 
          <Link to='/cart'><img src={cart_icon} alt="" /></Link>
         <div className="nav-cart-count">0</div>
       </div>
